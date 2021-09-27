1871f6bce908566a85c9439f8ae2cb08d38e8ad8
Juciano <juciano.junior@hotmail.com>
2021-09-27 19:05:46 +0100

    Estrutura inicial do projeto

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+//const path = require('path')
+
+//const db = require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+// conexao com o banco de dados
+//db.connect()
+
+// habilitando o server para receber dados via post (Formulário)
+app.use(express.urlencoded( {extended: true} ))
+
+// Definindo as rotas
+app.use('/api', routes)
+
+//executando servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
